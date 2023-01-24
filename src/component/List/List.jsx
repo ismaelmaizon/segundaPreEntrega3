636fdd4bed2beafc6d3c3b0a580ace39bdb19c6e
ismaelmaizon <108308275+ismaelmaizon@users.noreply.github.com>
2023-01-23 23:14:07 -0300

    Add files via upload

--- src/component/List/List.jsx
+++ src/component/List/List.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import { useEffect } from 'react';
 import Card from '../Card/Card'
 
-const List = function ({data}) {
+const List = function ({data, x}) {
   
     return (
-          data.map(({idDrink, strDrink, strDrinkThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5}) => (
+          data[x].map(({idDrink, strDrink, strDrinkThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5}) => (
             <Card key={idDrink} name={strDrink} img={strDrinkThumb} description={[strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5]}/>))
     )
     
--- src/component/List/Listall.jsx
+++ src/component/List/Listall.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react';
-import Card from '../Card/Card'
+import { Link } from 'react-router-dom';
+import List from './List';
 
 const Listall = function ({data}) {
-  
+
     return (
-        data[1].map(({idDrink, strDrink, strDrinkThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5}) => (
-            <Card key={idDrink} name={strDrink} img={strDrinkThumb} description={[strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5]}/>
-            )))
+        <div>
+            <List data={data} x={0}/> 
+            <List data={data} x={1}/>
+            <List data={data} x={2}/> 
+        </div>
+    )
+        
   }
 
 
@@ -15,3 +20,19 @@ export default Listall;
 
 
 // idDrink, strDrink, strDrinkThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5
+// 
+{/* <div className='card'>
+            <div className='cardTitle'>
+                <h1>{data[0][0].strDrink}</h1>
+            </div>
+            <div>
+                <img className='cardImg' src={data[0][0].strDrinkThumb} alt="" />
+            </div>
+            <div className='cardInfo'>
+                <h2>Ingredientes:</h2>
+                <p>{data[0][0].strIngredient1+", "+data[0][0].strIngredient2+", "+data[0][0].strIngredient3+", "+data[0][0].strIngredient4+", "+data[0][0].strIngredient5}</p>
+            </div>
+            <Link to={`${data[0][0].strDrink}`} >
+                <button className='btn'>mas info</button>
+            </Link>
+        </div>) */}
